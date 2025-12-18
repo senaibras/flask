b606857d33d1075dfcbfdc5a2775b724c6e19132
Professor Leandro Soares <senaispbras@gmail.com>
2025-12-18 10:05:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ from categoria_controller import categoria_bp
 app = Flask(__name__)
 
 app.register_blueprint(categoria_bp)
+app.register_blueprint(produto_bp)
 
 @app.route("/")
 def home():
     return {"status": "API rodando 🚀"}
+
 
